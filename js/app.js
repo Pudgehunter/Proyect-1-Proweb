f18017fc87e854697902228d0b26f59dfb9e6f2c
Yoshiki Tsutsui <pudgehunter2@hotmail.com>
2021-09-19 23:35:14 -0500

    putting some sliders for interactions

--- js/app.js
+++ js/app.js
@@ -7,3 +7,20 @@ window.onscroll = function(e) {
         menu.classList.remove('menu--scroll');
     }
 }
+
+var swiper = new Swiper(".mySwiper", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
