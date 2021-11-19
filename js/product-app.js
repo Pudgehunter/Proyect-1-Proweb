6e020393c107bd379584c8d00cbe8a2f6803ec0d
Yoshiki Tsutsui <pudgehunter2@hotmail.com>
2021-11-18 22:32:59 -0500

    making more easier the css part

--- js/product-app.js
+++ js/product-app.js
@@ -6,9 +6,6 @@ const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 const auth = getAuth();
 
-
-
-
 window.onscroll = function (e) {
     const posY = document.documentElement.scrollTop;
     if (posY >= 150) {
