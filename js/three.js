66b589392e7a994aae38408b7629f420e1064b5b
Yoshiki Tsutsui <pudgehunter2@hotmail.com>
2021-12-09 21:20:23 -0500

    Library works, but he has his own life

--- js/three.js
+++ js/three.js
@@ -6,37 +6,45 @@ import { GLTFLoader } from 'https://cdn.skypack.dev/three@0.135.0/examples/jsm/l
 // Our Javascript will go here.
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+//camera.position.set = (0.025,0.025,0.025);
+camera.position.x = 0;
+camera.position.y = 0;
+camera.position.z = 5;
+scene.add(camera);
 
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
+
+renderer.gammaOuyput = true;
 document.body.appendChild( renderer.domElement );
 
-const geometry = new THREE.BoxGeometry();
-const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-const cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
-
-camera.position.z = 5;
-
-const animate = function () {
-    requestAnimationFrame( animate );
-
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
-
-    renderer.render( scene, camera );
-};
-
-animate();
+//const light = new THREE.DirectionalLight(0xffffff, 1);
+//light.position.set(2,2,5);
+//scene.add(light);
 
 const loader = new GLTFLoader();
 
-loader.load( 'scene.glb', function ( gltf ) {
+let rice;
 
-	scene.add( gltf.scene );
+loader.load( 'models/onigiris/scene.gltf', function (gltf) {
+
+    //console.log(gltf);
+    rice = gltf.scene;
+	scene.add( rice );
 
 }, undefined, function ( error ) {
 
 	console.error( error );
 
 } );
+
+function animate(){
+    requestAnimationFrame(animate);
+
+    rice.rotation.x += 0.01;
+	rice.rotation.y += 0.01;
+
+    renderer.render(scene, camera);
+}
+
+animate();
